3c00f0022fcdc00de6db6253c06b1e92c32e8f51
Namsojeong <nsj050320@gmail.com>
2022-10-05 08:13:45 +0900

    22-10-05

--- CodingTest2/Cote2/Test.cpp
+++ CodingTest2/Cote2/Test.cpp
@@ -1,18 +1,15 @@
 #include<iostream>
 
 using namespace std;
-int n;
 
 int main()
 {
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-	cin >> n;
 
-	if (n >= 90) cout << 'A';
-	else if (n >= 80) cout << 'B';
-	else if (n >= 70) cout << 'C';
-	else if (n >= 60) cout << 'D';
-	else cout << 'F';
+	cout << "\\    /\\" << '\n';
+	cout << " )  ( ')" << '\n';
+	cout << "(  /  )"<<'\n';
+	cout << " \\(__)|" << '\n';
 
 	return 0;
 }
