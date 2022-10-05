aa29d986ecb2b1b0d62ab2113e1a3d63bdb02ad7
Namsojeong <nsj050320@gmail.com>
2022-10-05 14:57:06 +0900

    22-10-05 문제 더 품

--- CodingTest2/Cote2/Test.cpp
+++ CodingTest2/Cote2/Test.cpp
@@ -1,15 +1,19 @@
 #include<iostream>
 
 using namespace std;
+int n, k;
 
+int Fact(int num, int all, int cnt)
+{
+	if (cnt == 0) return all;
+	return Fact(num-1, all*num, cnt-1);
+}
 int main()
 {
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
-	cout << "\\    /\\" << '\n';
-	cout << " )  ( ')" << '\n';
-	cout << "(  /  )"<<'\n';
-	cout << " \\(__)|" << '\n';
+	cin >> n >> k;
+	cout << Fact(n, 1, k) / Fact(k, 1, k);
 
 	return 0;
 }
