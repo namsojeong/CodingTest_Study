755679686eef4d3cd3f6de3c3e867f012effa3b5
Namsojeong <nsj050320@gmail.com>
2022-10-11 09:40:39 +0900

    22-10-11

--- CodingTest2/Cote2/Test.cpp
+++ CodingTest2/Cote2/Test.cpp
@@ -1,42 +1,18 @@
 #include<iostream>
 
 using namespace std;
-
-int a, b;
-int r = 1;
-bool isFind = false;
+string str;
 
 int main()
 {
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
-	int n;
-	cin >> n;
-	for (int i = 0; i < n; i++)
+	cin >> str;
+	for (int i = 0; i < str.size(); i++)
 	{
-		r = 1;
-		cin >> a >> b;
-
-		if (a > b) swap(a, b);
-
-		while (a > 1 && b > 1)
-		{
-			isFind = false;
-			for (int i = 2; i <= a; i++)
-			{
-				if (a % i == 0 && b % i == 0)
-				{
-					isFind = true;
-					a /= i;
-					b /= i;
-					r *= i;
-					break;
-				}
-				else isFind = false;
-			}
-			if (!isFind) break;
-		}
-		cout << r * a * b<<'\n';
+		if (isupper(str[i])) str[i] = tolower(str[i]);
+		else if(islower(str[i])) str[i] = toupper(str[i]);
 	}
-	
+
+	cout << str;
 }
