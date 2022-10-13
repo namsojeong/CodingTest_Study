a4aed65215e623d04c1867e866ef11262a175382
Namsojeong <nsj050320@gmail.com>
2022-10-13 19:50:29 +0900

    22-10-14

--- CodingTest2/Cote2/Test.cpp
+++ CodingTest2/Cote2/Test.cpp
@@ -8,12 +8,11 @@ int main()
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
 	cin >> n;
-	for (int i = 0; i <n; i++)
+	for (int i = n-1; i >=0; i--)
 	{
-		for (int j = n-i; j >0; j--)
-		{
-			cout << "*";
-		}
+		for (int j = 0; j < i; j++) cout << " ";
+		for (int j = 0; j < n - i; j++) cout << "*";
+		for (int j = 0; j < n - i-1; j++) cout << "*";
 		cout << '\n';
 	}
 	
