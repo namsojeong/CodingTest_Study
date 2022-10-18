495fee6d071e3f381f0f3dc7b647f83e7232a6c0
Namsojeong <nsj050320@gmail.com>
2022-10-19 00:19:03 +0900

    22-10-19

---
+++ CodingTest2/Cote2/별찍기0.cpp
@@ -0,0 +1,15 @@
+//#include<iostream>
+//
+//using namespace std;
+//int n;
+//
+//int main()
+//{
+//	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+//
+//	cin >> n;
+//	for (int i = n; i > 0; i--)
+//	{
+//		cout << "*\n";
+//	}
+//}
