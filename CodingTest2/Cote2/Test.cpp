5508ed66b54e2a7376cae439a6c62a381aa94c7a
Namsojeong <nsj050320@gmail.com>
2022-10-29 09:45:28 +0900

    22-10-29

--- CodingTest2/Cote2/Test.cpp
+++ CodingTest2/Cote2/Test.cpp
@@ -1,18 +1,33 @@
-#include<iostream>
-
+#include <iostream>
 using namespace std;
-int n, result=0;
 
-int main()
-{
-	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+int a, b, c, cnt = 0;
+int arr[7] = { 0, };
+int n = 0;
 
-	cin >> n;
+int main() {
+    cin >> a>>b>>c;
 
-	for (int i = 1; i <= n; i++)
-	{
-		result += i;
-	}
+    arr[a]++;
+    arr[b]++;
+    arr[c]++;
 
-	cout << result;
+    for (int i = 1; i <= 6; i++)
+    {
+        if (arr[i] >= 2)
+        {
+            n = i;
+            cnt++;
+            if (arr[i] == 3)cnt++;
+        }
+    }
+
+    if (cnt == 0)
+    {
+        int maxNum = max(a, b);
+        maxNum = max(maxNum, c);
+        cout <<  maxNum * 100;
+    }
+    else if (cnt == 1) cout << 1000 + n * 100;
+    else if (cnt == 2) cout << 10000 + n * 1000;
 }
