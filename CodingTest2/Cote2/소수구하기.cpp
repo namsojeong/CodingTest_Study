292ed518136beabbed0e63d0478b1004344e4eb1
Namsojeong <nsj050320@gmail.com>
2022-11-04 01:38:21 +0900

    22-11-04

--- CodingTest2/Cote2/소수구하기.cpp
+++ CodingTest2/Cote2/소수구하기.cpp
@@ -1,25 +1,28 @@
 #include<iostream>
+#include<math.h>
 
 using namespace std;
 
-int a, b, n;
-bool isS = false;
+int a, b;
+bool isCor[1000001];
 
 int main()
 {
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
 	cin >> a >> b;
-	n = a;
-	while (n <= b)
-	{
-		for (int j = 2; j < n; j++)
-		{
-			isS = false;
-			if (n % j == 0 && n != j) break;
-			isS = true;
+	fill_n(isCor, b + 1, true);
+
+	isCor[0] = false;
+	isCor[1] = false;
+
+	for (int i = 2; i <= sqrt(b); i++) {
+		if (isCor[i] == true) {
+			for (int j = i * 2; j <= b; j += i) isCor[j] = false;
 		}
-		if (isS) cout << n << '\n';
-		n++;
+	}
+
+	for (int i = a; i <= b; i++) {
+		if (isCor[i]) cout  << i<<'\n';
 	}
 }
