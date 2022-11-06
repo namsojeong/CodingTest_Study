590f46cb201af6169483059f8812fdcbecd1f8a1
Namsojeong <nsj050320@gmail.com>
2022-11-06 19:20:19 +0900

    22-11-06

--- CodingTest2/Cote2/Test.cpp
+++ CodingTest2/Cote2/Test.cpp
@@ -1,33 +1,8 @@
 #include <iostream>
 using namespace std;
 
-int a, b, c, cnt = 0;
-int arr[7] = { 0, };
-int n = 0;
-
+int a, s;
 int main() {
-    cin >> a>>b>>c;
-
-    arr[a]++;
-    arr[b]++;
-    arr[c]++;
-
-    for (int i = 1; i <= 6; i++)
-    {
-        if (arr[i] >= 2)
-        {
-            n = i;
-            cnt++;
-            if (arr[i] == 3)cnt++;
-        }
-    }
-
-    if (cnt == 0)
-    {
-        int maxNum = max(a, b);
-        maxNum = max(maxNum, c);
-        cout <<  maxNum * 100;
-    }
-    else if (cnt == 1) cout << 1000 + n * 100;
-    else if (cnt == 2) cout << 10000 + n * 1000;
+    cin >> a>>s;
+    cout << (s * 2) - a;
 }
