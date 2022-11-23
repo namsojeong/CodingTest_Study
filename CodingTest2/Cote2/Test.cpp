1456c14b7b4a74ae35b6c1ca9db8266d3f23f6ee
Namsojeong <nsj050320@gmail.com>
2022-11-24 00:40:13 +0900

    22-11-24

--- CodingTest2/Cote2/Test.cpp
+++ CodingTest2/Cote2/Test.cpp
@@ -1,33 +1,8 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
-
-int total, n, m, cnt, num;
-vector<int> vec;
-double result;
+string n;
 int main() {
 	cin >> n;
-
-	for (int i = 0; i < n; i++) {
-		total = 0, cnt = 0;
-		vec.clear();
-		cin >> m;
-
-		for (int j = 0; j < m; j++) {
-			cin >> num;
-			total += num;
-			vec.push_back(num);
-		}
-		total /= m;
-
-		for (int j = 0; j < m; j++) {
-			if (vec[j] > total) cnt++;
-		}
-		result = (double)cnt / m * 100;
-
-		cout << fixed;
-		cout.precision(3);
-		cout << result << "%\n";
-	}
+	cout << n + "??!";
 }
