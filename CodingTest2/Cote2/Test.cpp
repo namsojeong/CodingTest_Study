5d80b74180af47268ef5bacc04d65cad615ff5d6
Namsojeong <nsj050320@gmail.com>
2022-11-28 08:48:38 +0900

    22-11-28

--- CodingTest2/Cote2/Test.cpp
+++ CodingTest2/Cote2/Test.cpp
@@ -1,17 +1,43 @@
 #include <iostream>
-#include <set>
+#include <string>
+#include <vector>
+
 using namespace std;
 
-int num;
-set<int> s;
+int a, b;
+vector <int> va;
+vector <int> vb;
 int main() {
 	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
-	for (int i = 0; i < 10; i++)
-	{
-		cin >> num;
-			s.insert(num%42);
-	}
+	cin >> a>>b;
 
-	cout << s.size();
+	while (a > 0)
+	{
+		va.push_back(a % 10);
+		a /= 10;
+	}
+	while (b > 0)
+	{
+		vb.push_back(b % 10);
+		b /= 10;
+	}
+	a = b = 0;
+	int temp = 1;
+	while (!va.empty())
+	{
+		a += va.back()*temp;
+		temp *= 10;
+		va.pop_back();
+	}
+	temp = 1;
+	while (!vb.empty())
+	{
+		b += vb.back() * temp;
+		temp *= 10;
+		vb.pop_back();
+	}
+	int maxNum = a>b?a:b;
+
+	cout << maxNum;
 }
