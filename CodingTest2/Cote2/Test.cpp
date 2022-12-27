17d9fa521304c4c2477df1b4ad25e412aa55f3da
Namsojeong <nsj050320@gmail.com>
2022-12-28 08:22:27 +0900

    22-12-28

--- CodingTest2/Cote2/Test.cpp
+++ CodingTest2/Cote2/Test.cpp
@@ -1,30 +1,18 @@
 #include <iostream>
-#include <map>
 using namespace std;
 
-string str;
-int arr[27] = { -1,-1, -1, -1,  };
+int n;
+int fibo(int a)
+{
+	if (a == 0) return 0;
+	if (a == 1) return 1;
+	return fibo(a-2) + fibo(a - 1);
+}
 int main()
 {
 	ios::sync_with_stdio(false); cin.tie(NULL);  cout.tie(NULL);
-	fill_n(arr, 26, -1);
+	
+	cin >> n;
+	cout << fibo(n);
 
-	cin >> str;
-
-	for (int i = 0; i < str.length(); i++)
-	{
-		if (arr[str[i] - 97] == -1)
-		{
-			arr[str[i] - 97] = i;
-			cout << str[i]-97 << endl;
-		}
-	}
-
-
-	for (int i = 0; i < 26; i++)
-	{
-		cout << arr[i]<<" ";
-	}
-
-	return 0;
 }
