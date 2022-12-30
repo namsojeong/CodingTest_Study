153e8feb26f26ab3d8bc4d8dadfba687dce7566e
Namsojeong <nsj050320@gmail.com>
2022-12-30 18:20:54 +0900

    22-12-30

--- CodingTest2/Cote2/Test.cpp
+++ CodingTest2/Cote2/Test.cpp
@@ -2,20 +2,11 @@
 using namespace std;
 
 int n;
-int arr[46] = { };
-int fibo(int a)
-{
-	if (arr[a] == -1)	arr[a] = fibo(a - 2) + fibo(a - 1);
-	return arr[a];
-}
 int main()
 {
 	ios::sync_with_stdio(false); cin.tie(NULL);  cout.tie(NULL);
-	fill_n(arr, 46, -1);
-	arr[0] = 0;
-	arr[1] = 1;
 
 	cin >> n;
-	cout << fibo(n);
+	cout << n - 543;
 
 }
