45284aba750511a731c5147d49c09991c250c222
Namsojeong <nsj050320@gmail.com>
2022-12-31 18:04:52 +0900

    22-12-31

--- CodingTest2/Cote2/Test.cpp
+++ CodingTest2/Cote2/Test.cpp
@@ -2,11 +2,15 @@
 using namespace std;
 
 int n;
+string str;
 int main()
 {
 	ios::sync_with_stdio(false); cin.tie(NULL);  cout.tie(NULL);
 
 	cin >> n;
-	cout << n - 543;
-
+	for (int i = 0; i < n; i++)
+	{
+		cin >> str;
+		cout << str[0] << str[str.size() - 1]<<"\n";
+	}
 }
