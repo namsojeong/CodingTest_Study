03212b0d02713c207998fe77841dbd6dfc3f9119
Namsojeong <nsj050320@gmail.com>
2023-01-02 08:35:08 +0900

    23-01-02

--- CodingTest2/Cote2/Test.cpp
+++ CodingTest2/Cote2/Test.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-long long n, m;
+vector<pair<int, int>> v;
+int n, m;
 int main()
 {
 	ios::sync_with_stdio(false); cin.tie(NULL);  cout.tie(NULL);
 
-	cin >> n >> m;
-	long long temp = n - m;
-	temp = temp < 0 ? temp * -1 : temp;
-	cout << temp;
+	for (int i = 0; i < 3; i++)
+	{
+		cin >> n, m;
+		v.push_back({ n, m });
+	}
+
+
+
 }
