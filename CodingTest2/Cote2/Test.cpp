4f2771e874659cb28e86ae0c25b277c0d5f12ee7
Namsojeong <nsj050320@gmail.com>
2023-01-04 09:02:50 +0900

    23-01-04

--- CodingTest2/Cote2/Test.cpp
+++ CodingTest2/Cote2/Test.cpp
@@ -1,17 +1,13 @@
 #include <iostream>
 using namespace std;
 
-int fac(int a)
-{
-	if (a == 1) return 1;
-	if (a == 0) return 1;
-	return fac(a - 1) * fac(a - 2);
-}
-int n;
+int n, m;
 int main()
 {
 	ios::sync_with_stdio(false); cin.tie(NULL);  cout.tie(NULL);
 
-	cin >> n;
-
+	cout << "#  # #### #### #  #\n";
+	cout << "#### #  # #  # # #\n";
+	cout << "#### #  # #  # # #\n";
+	cout << "#  # #### #### #  #";
 }
